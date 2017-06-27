fd5ae3eb9d69668f26c5fd936699c26003a293f1
litao <731334268@qq.com>
2017-06-27 18:22:05 +0800

    jsonp 支持

--- java/src/main/java/com/view/JsonpAdvice.java
+++ java/src/main/java/com/view/JsonpAdvice.java
@@ -1,6 +1,5 @@
 package com.view;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
 
 /**
