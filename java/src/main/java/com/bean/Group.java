d38599cddfbd6d2d7137d6a704f7217a2022c0f7
litao <731334268@qq.com>
2017-07-25 09:39:56 +0800

    fastjson学习

---
+++ java/src/main/java/com/bean/Group.java
@@ -0,0 +1,47 @@
+package com.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <p>Title:      fastjson测试对象. </p>
+ * <p>Description TODO </p>
+ * <p>Company:    https://www.sogou.com/ </p>
+ *
+ * @Author <a href="litaoos2862@sogou-inc.com"/>李涛</a>
+ * @CreateDate 2017/7/24 17:11
+ */
+public class Group {
+
+    private Long       id;
+    private String     name;
+    private List<User> users = new ArrayList<User>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+}
